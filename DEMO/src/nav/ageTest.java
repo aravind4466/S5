a60ceced4446777b43fcc009d54f9cf642126c26
Satya <Satya@DESKTOP-GH4UT8U>
2020-02-03 11:55:11 +0530

    my parameterized class

--- DEMO/src/nav/ageTest.java
+++ DEMO/src/nav/ageTest.java
@@ -2,26 +2,44 @@ package nav;
 
 import static org.junit.Assert.assertEquals;
 
+import java.util.Arrays;
+import java.util.Collection;
+
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 
-public class ageTest {
+@RunWith(Parameterized.class)
+public class ageTest 
+{
+	private int input;
+	private String output;
+	public ageTest(int input,String output)
+	{
+		this.input=input;
+		this.output=output;
+	}
 	age a;
 	@Before
 	public void init()
 	{
 		a=new age();
 	}
-
-	@Test
-	public void test() {
-		age a = new age();
-		assertEquals("eligible to vote",a.election(20));
+	@Parameterized.Parameters
+	public static Collection aget() 
+	{
+		return Arrays.asList(new Object [][]{
+				{14,"not eligible to vote"},
+				{20,"eligible to vote"}
+		}
+		);
 	}
 	@Test
-	public void test1() {
-		age a = new age();
-		assertEquals("eligible to vote",a.election(21));
-	}
+	public void address()
+	{
+	assertEquals(output,a.election(input));
+	
+	}	}
 
-}
+
